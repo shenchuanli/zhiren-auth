ca9f26cb92f7eac016652bd8308592c641a55a5d
MrHKing <792679341@qq.com>
2018-04-29 10:18:16 +0800

    修改bug OncePerRequestFilter替换GenericFilterBean，解决filter触发多次问题

--- zhiren-auth-web/src/main/java/cn/zhiren/auth/filter/JWTAuthenticationFilter.java
+++ zhiren-auth-web/src/main/java/cn/zhiren/auth/filter/JWTAuthenticationFilter.java
@@ -5,13 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -20,15 +19,11 @@ import java.util.Collection;
  * Created by Solley
  * Created Time  2018/4/26.
  */
-public class JWTAuthenticationFilter extends GenericFilterBean {
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain filterChain)
-            throws IOException, ServletException {
-
-        String token = ((HttpServletRequest)request).getHeader("Authorization");
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = httpServletRequest.getHeader("Authorization");
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = null;
         if (token != null) {
             //查询鉴权服务器并获得 相关的资源权限
@@ -36,7 +31,7 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
             Collection<GrantedAuthority> authorities = new ArrayList();
             authorities.addAll(authentication.getAuthorities());
             for(int i = 0; i < authorities.size();i++) {
-                System.out.println(((SimpleGrantedAuthority)((ArrayList<GrantedAuthority>) authorities).get(i)).getAuthority());
+                System.out.println(((ArrayList<GrantedAuthority>) authorities).get(i).getAuthority());
             }
             authorities.add(new SimpleGrantedAuthority("ROLE_GET"));
             System.out.println(authentication.getPrincipal());
@@ -44,6 +39,6 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
         }
         SecurityContextHolder.getContext()
                 .setAuthentication(usernamePasswordAuthenticationToken);
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
     }
 }
