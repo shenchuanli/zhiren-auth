db9545957f5f680b6bdcecdf9092030279df2259
MrHKing <792679341@qq.com>
2018-05-03 20:42:05 +0800

    添加注册中心客户端和Feign

--- zhiren-auth-web/src/main/java/cn/zhiren/auth/ZhirenApplication.java
+++ zhiren-auth-web/src/main/java/cn/zhiren/auth/ZhirenApplication.java
@@ -3,7 +3,11 @@ package cn.zhiren.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 @EnableCaching
 public class ZhirenApplication {
